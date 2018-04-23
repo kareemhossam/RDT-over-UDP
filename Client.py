e9d207ca0600415c9fd9add63353dfd92794afd1
ahmedibraheeem <ahmedibraheeem100@gmail.com>
2018-04-23 21:56:12 +0200

    the code fucking works

--- Client.py
+++ Client.py
@@ -1,27 +1,9 @@
 from socket import *
-'''import numpy as np
-
-max_packet_length = 500
-
-class Packet:
-    cksum = np.uint16(None)
-    length = np.uint16(None)
-    seqno = np.uint16(None)
-    data = "\0" * (max_packet_length -8)
-
-class ack_packet:
-    cksum = np.uint16(None)
-    length = np.uint16(None)
-    ackno = np.uint16(None)
-
-def get_arg(max,seed,p):
-'''
-#this is a test 
-serverName = 'hostname'
+serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = input("Input lowercase sentence:")
-clientSocket.sendto(message.encode(), (serverName, serverPort))
+message = input('Input lowercase sentence:').encode('ascii')
+clientSocket.sendto(message, (serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print(modifiedMessage.decode())
+print(modifiedMessage.decode('ascii'))
 clientSocket.close()

597777eed87d3b9c3b5a0e27da8ee35e047799b7
ahmedibraheeem <ahmedibraheeem100@gmail.com>
2018-04-23 22:38:07 +0200

    UDP done

--- Server.py
+++ Server.py
@@ -4,7 +4,13 @@ serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 print("The server is ready to receive")
 while True:
+	
     message, clientAddress = serverSocket.recvfrom(2048)
     modifiedMessage = message.upper()
     serverSocket.sendto(modifiedMessage, clientAddress)
-    print(modifiedMessage.decode('ascii'))	
+    print('recieving message')
+    print('recieved {0} bytes from ${1}'.format(len(modifiedMessage), clientAddress))
+    print('the message is:'+ modifiedMessage.decode('ascii'))
+    if modifiedMessage:	
+    	sent=serverSocket.sendto(modifiedMessage, clientAddress)
+    	print('{0} bytes are sent back to the client {1}'.format(sent,clientAddress))

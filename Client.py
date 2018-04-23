e00c24e2f9f8a26420d3d4f8d359166410122fba
kareemhossam <es-kareem.hossam@alexu.edu.eg>
2018-04-23 20:56:25 +0200

    removed all commented lines

--- Client.py
+++ Client.py
@@ -1,22 +1,5 @@
 from socket import *
-'''import numpy as np
 
-max_packet_length = 500
-
-class Packet:
-    cksum = np.uint16(None)
-    length = np.uint16(None)
-    seqno = np.uint16(None)
-    data = "\0" * (max_packet_length -8)
-
-class ack_packet:
-    cksum = np.uint16(None)
-    length = np.uint16(None)
-    ackno = np.uint16(None)
-
-def get_arg(max,seed,p):
-'''
-#this is a test 
 serverName = 'hostname'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
